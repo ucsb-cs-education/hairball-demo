d2ad1e4c5b94c4459baa0ee1d423bf9be060632b
Bryce Boe <bbzbryce@gmail.com>
2013-02-27 20:16:25 -0800

    Add google analytics.

--- app.py
+++ app.py
@@ -243,6 +243,16 @@ dynamically view new projects.</noscript>
       e.target.value = '';
   });
   sse();
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-510348-18']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
 </script>
 """ % '\n'.join(images)
 
